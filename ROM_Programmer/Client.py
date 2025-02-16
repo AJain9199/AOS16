240601be92d031f2ebfa6a0d41e3f2c0ca1d88d3
Aryaman Jain <aryamanjain9199@gmail.com>
2025-02-16 17:08:06 +0530

    Completed ROM write functionality

--- ROM_Programmer/Client.py
+++ ROM_Programmer/Client.py
@@ -1,13 +1,55 @@
 import serial
 import time
-SerialObj = serial.Serial('COM9')
-SerialObj.baudrate = 9600  # set Baud rate to 9600
-SerialObj.bytesize = 8   # Number of data bits = 8
-SerialObj.parity  ='N'   # No parity
-SerialObj.stopbits = 1   # Number of Stop bits = 1
+
+srl = serial.Serial('COM9')
+srl.baudrate = 9600  # set Baud rate to 9600
+srl.bytesize = 8   # Number of data bits = 8
+srl.parity  ='N'   # No parity
+srl.stopbits = 1   # Number of Stop bits = 1
+
+def writeData(addr, data): 
+    srl.write(bytearray([addr >> 8, addr & 0xff, data]))
+    time.sleep(0.001)
+    return int.from_bytes(srl.read(1)) == data
+    
+def writeBlock(data):
+    srl.write('w'.encode())
+    srl.write(bytearray([(data >> 8), data & 0xff]))
+    time.sleep(0.1)
+    for (addr, d) in data:
+        writeData(addr, d)
+
 time.sleep(3)
 
-data = SerialObj.read(1);
-print(int.from_bytes(data));
-data = SerialObj.read(1);
-print(int.from_bytes(data));
+SerialObj.write('c'.encode());
+
+time.sleep(0.1);
+manufacturer_id = int.from_bytes(SerialObj.read(1));
+chip = int.from_bytes(SerialObj.read(1));
+
+if manufacturer_id == 0xBF and chip == 0xB5:
+    print("Manufacturer handshake successful.")
+
+print("Initiating Chip Erase.")
+SerialObj.write('e'.encode())
+time.sleep(10);
+response = int.from_bytes(SerialObj.read(1));
+if response == 0xFF:
+    print("Chip Erase Successful.")
+
+SerialObj.write('w'.encode());
+SerialObj.write(bytearray([0, 2]));
+print(SerialObj.readline());
+
+SerialObj.write([0, 0, 1]);
+SerialObj.write([0, 1, 2]);
+
+time.sleep(1)
+
+print(int.from_bytes(SerialObj.read(1)));
+
+time.sleep(1);
+
+SerialObj.write('r'.encode())
+time.sleep(0.1);
+print(int.from_bytes(SerialObj.read(1)));
--- ROM_Programmer/ROM_Programmer.ino
+++ ROM_Programmer/ROM_Programmer.ino
@@ -27,6 +27,21 @@ const uint16_t CHIP_ID_EXIT[][2] {
   {0x5555, 0xF0}
 };
 
+const uint16_t CHIP_ERASE[][2] {
+  {0x5555, 0xAA},
+  {0x2AAA, 0x55},
+  {0x5555, 0x80},
+  {0x5555, 0xAA},
+  {0x2AAA, 0x55},
+  {0x5555, 0x10}
+};
+
+const uint16_t CHIP_WRITE[][2] {
+  {0x5555, 0xAA},
+  {0x2AAA, 0x55},
+  {0x5555, 0xA0}
+};
+
 void setAddr(uint16_t addr) {
   digitalWrite(LATCH, LOW);
   shiftOut(DATA, CLK, MSBFIRST, (uint8_t)(addr >> 8));
@@ -62,17 +77,21 @@ void read_mode() {
   SET_OE(LOW); SET_WE(HIGH);
 }
 
+void write_cycle_step(uint16_t addr, uint8_t data) {
+  setAddr(addr); dataOut(data); SET_WE(HIGH); SET_WE(LOW); SET_WE(LOW); SET_WE(HIGH);
+}
+
 void write_cycle(uint16_t **cmd, int steps) {
   SET_OE(HIGH);
 
   for (int i = 0; i < steps; i++) {
-    setAddr(cmd[i][0]); dataOut((uint8_t)cmd[i][1]); SET_WE(HIGH); SET_WE(LOW); SET_WE(LOW); SET_WE(HIGH);
+    write_cycle_step(cmd[i][0], cmd[i][1]);
   }
-  delay(10);
 }
 
 void chipId() {
   write_cycle((uint16_t **)CHIP_ID_ENTRY, 3);
+  delay(5);
   setAddr(0x0);
   Serial.write(dataIn());
 
@@ -80,6 +99,35 @@ void chipId() {
   Serial.write(dataIn());
 
   write_cycle((uint16_t **) CHIP_ID_EXIT, 3);
+}
+
+void chipErase() {
+  write_cycle((uint16_t **) CHIP_ERASE, 6);
+  SET_OE(LOW);
+  int c = 0; while ((dataIn() & 128) != 128 && c < 2000) { c++; delayMicroseconds(100); }
+  SET_OE(HIGH);
+  if (c < 2000) {
+  Serial.write(0xFF);
+  }
+}
+
+void chipWrite(uint16_t addr, uint8_t data) {
+  SET_WE(HIGH);
+  SET_OE(HIGH);
+  setAddr(0x5555); dataOut(0xaa); SET_WE(HIGH); SET_WE(LOW); SET_WE(LOW); SET_WE(HIGH);
+  setAddr(0x2aaa); dataOut(0x55); SET_WE(HIGH); SET_WE(LOW); SET_WE(LOW); SET_WE(HIGH);
+  setAddr(0x5555); dataOut(0xa0); SET_WE(HIGH); SET_WE(LOW); SET_WE(LOW); SET_WE(HIGH);
+  setAddr(addr); dataOut(data); SET_WE(HIGH); SET_WE(LOW); SET_WE(LOW); SET_WE(HIGH);
+  read_mode();
+
+  SET_OE(LOW);
+  int c = 0; while (((dataIn()&128) != (data&128)) && (c < 100)) c++;
+  SET_OE(HIGH);
+  if (c < 100) {
+    Serial.write(data);
+  } else {
+    Serial.write(~data);
+  }
 }
 
 void setup() {
@@ -91,10 +139,41 @@ void setup() {
   pinMode(A1, OUTPUT);
 
   Serial.begin(9600);
-
-  chipId();
 }
 
 void loop() {
+  if (Serial.available() <= 0) {
+    return;
+  }
 
+  switch (Serial.read()) {
+    case 'c':
+      chipId();
+      break;
+    case 'e':
+      chipErase();
+      return;
+    case 'w': {
+      delay(10);
+      byte buf[3] = {0};
+      Serial.readBytes(buf, 2);
+      uint16_t len = (buf[0] << 8) | buf[1];
+      for (int i = 0; i < len; i++) {
+        Serial.readBytes(buf,3);
+        uint16_t addr = (buf[0] << 8) | buf[1];
+        uint8_t data = buf[2];
+
+        chipWrite(addr, data);
+      }
+      
+      return;
+    }
+
+    case 'r':
+    read_mode();
+      setAddr(0);
+      delay(10);
+      Serial.write(dataIn());
+      break;
+  }
 }
