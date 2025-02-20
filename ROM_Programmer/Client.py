7ecd12ac62bac731cb867fc99f612a5fe486438a
Aryaman Jain <aryamanjain9199@gmail.com>
2025-02-20 21:41:47 +0530

    Increased flash programmer performance

--- ROM_Programmer/Client.py
+++ ROM_Programmer/Client.py
@@ -1,11 +1,19 @@
 import serial
 import time
+import sys
+import argparse
+from argparse import ArgumentParser
+
+
+CHUNK_SIZE = 1024
 
 def read8(srl):
     return int.from_bytes(srl.read(1))
 
+
 def read32(srl):
-    return (read8(srl) << 24) | (read8(srl) << 16) | (read8(srl) << 8) | read8(srl) 
+    return (read8(srl) << 24) | (read8(srl) << 16) | (read8(srl) << 8) | read8(srl)
+
 
 def manufacturer_id_handshake(srl):
     srl.write('c'.encode())
@@ -18,6 +26,7 @@ def manufacturer_id_handshake(srl):
     else:
         return
 
+
 def chip_erase(srl):
     srl.write('e'.encode())
     time.sleep(0.5)
@@ -26,59 +35,115 @@ def chip_erase(srl):
     else:
         print("Chip erase failed.")
 
+
 def write32(srl, n):
-    srl.write(bytearray([(n >> 24) & 0xff, (n >> 16) & 0xff, (n >> 8) & 0xff, n & 0xff]))
+    srl.write(n.to_bytes(4, 'big'))
+
 
 def write8(srl, n):
     srl.write(n)
 
-def write_addr(srl, data):
+
+def write_data(srl, data):
     srl.write('w'.encode())
     srl.write(len(data).to_bytes(4, 'big'))
     srl.write('a'.encode())
 
-    for (addr, byte) in data:
-        print(byte)
-        srl.write(addr.to_bytes(4, 'big'))
-        srl.write(byte.to_bytes(1))
-        time.sleep(0.5)
+    bytedata = []
 
-        print(srl.readline())
-        print(srl.readline())
-    
-    if (read32(srl) == len(data)):
-        print("Write successful")
+    for (addr, byte) in data:
+        bytedata.extend(addr.to_bytes(4, 'big'))
+        bytedata.extend(byte.to_bytes(1))
+
+    srl.write(bytedata)
+
+    return read32(srl) == len(data)
+
+
+def write_seq_chunk(srl, b):
+    srl.write('w'.encode())
+    srl.write(len(b).to_bytes(4, 'big'))
+    srl.write('s'.encode())
+
+    print(b)
+    print(srl.readline())
+    srl.write(b)
+
+    time.sleep(1)
+
+    print(srl.readline())
+    return read32(srl) == len(b)
+
+
+def write_seq(srl, f):
+    while True:
+        chunk = f.read(CHUNK_SIZE)
+        if not chunk:
+            break
+        if not write_seq_chunk(srl, chunk):
+            return False
+        time.sleep(0.1)
+    return False
+
 
 def read_addr(srl, addrs):
     srl.write('r'.encode())
     write32(srl, len(addrs))
-    print(srl.readline())
     srl.write('a'.encode())
-    
-    data = []
+
+    bytedata = []
 
     for i in addrs:
-        write32(srl, i);
-    
-    for i in addrs:
-        data.append(read8(srl))
-    return data
+        bytedata.extend(i.to_bytes(4, 'big'))
 
-def main():
-    srl = serial.Serial('COM10')
-    srl.baudrate = 115200  # set Baud rate to 9600
-    srl.bytesize = 8   # Number of data bits = 8
-    srl.parity  ='N'   # No parity
-    srl.stopbits = 1   # Number of Stop bits = 1
+    srl.write(bytedata)
+
+    return list(srl.read(len(addrs)))
+
+
+def read_seq(srl, n):
+    srl.write('r'.encode())
+    write32(srl, n)
+    srl.write('s'.encode())
 
     time.sleep(1)
-    manufacturer_id_handshake(srl)
+    return list(srl.read(n))
 
-    chip_erase(srl);
 
-    write_addr(srl, [(2, 12)])
-    print(read_addr(srl, [2]))
-    
+def parse_paired(filename):
+    return [()]
+
+
+def parse_single(filename):
+    return []
+
 
 if __name__ == "__main__":
-    main()
+    # manufacturer_id()
+
+    srl = serial.Serial('COM10')
+    srl.baudrate = 115200  # set Baud rate to 9600
+    srl.bytesize = 8  # Number of data bits = 8
+    srl.parity = 'N'  # No parity
+    srl.stopbits = 1  # Number of Stop bits = 1
+
+    time.sleep(1)
+
+    parser = ArgumentParser()
+    parser.add_argument('-r', type=int, default=2048, help='Read from the flash sequentially.')
+    parser.add_argument('-ra', type=argparse.FileType('r'), help='Read from the flash using a file of addresses.')
+    parser.add_argument('-w', type=argparse.FileType('rb'),
+                        help='Write to the flash sequentially using data from a file.')
+    parser.add_argument('-wa', type=argparse.FileType('r'), help='Write to the flash address-wise using the file.')
+    parser.add_argument('-b', type=int, default=115200, help='Baud rate for serial communication. (default: 115200)')
+    parser.add_argument('-c', default='COM10', help='Serial port for communication. (default: COM10)')
+    args = parser.parse_args()
+    args = vars(args)
+
+
+    if args.get('w'):
+        chip_erase(srl)
+        print(args['w'])
+        write_seq_chunk(srl, [12, 2])
+        print(read_addr(srl, [0, 1]))
+    # main()
--- ROM_Programmer/ROM_Programmer.ino
+++ ROM_Programmer/ROM_Programmer.ino
@@ -1,87 +1,324 @@
-// Shift register connections
-#define DATA 11
-#define LATCH 8
-#define CLK 12
+/*
+ * PORTD: 21, 20, 19, 18, --, --, --, 38
+ * PORTE: 00, 01, --, 05, 02, 03, --, --
+ * PORTH: 17, 16, --, 06, 07, 08, 09, --
+ * PORTI: --, --, --, --, --, --, --, --
+ * PORTC: 37, 36, 35, 34, 33, 32, 31, 30
+ * PORTA: 22, 23, 24, 25, 26, 27, 28, 29
+ * PORTF: A0, A1, A2, A3, A4, A5, A6, A7
+ * PORTK: A8, A9, AA, AB, AC, AD, AE, AF
+ * PORTB: 53, 52, 51, 50, 10, 11, 12, 13
+ * PORTL: 49, 48, 47, 46, 45, 44, 43, 42
+ * PORTG: 41, 40, 39, --, --, --, --, --
+ *
+ *
+ */
 
-// Control pins
-#define WE 40
-#define OE 41
+#include "protocol.hpp"
+#include "crc32.hpp"
 
-#define SET_OE(state) digitalWrite(OE, state)
-#define SET_WE(state) digitalWrite(WE, state)
+#define DDR_ADDR_L   DDRA
+#define DDR_ADDR_M   DDRC
+#define DDR_ADDR_H   DDRG
+
+#define DDR_DATA_L   DDRL
+
+#define PORT_ADDR_L  PORTA
+#define PORT_ADDR_M  PORTC
+#define PORT_ADDR_H  PORTG
+
+#define PORT_DATA_L  PORTL
+#define PINP_DATA_L  PINL
+
+///////////////////////////////////
+
+#define DDR_WE       DDRD
+#define PORT_WE      PORTD
+#define MASK_WE      0x80u
+
+#define DDR_OE       DDRB
+#define PORT_OE      PORTB
+#define MASK_OE      0x01u
+
+//////////////////////////////
+
+#define DATA_DIR_OUT    1
+#define DATA_DIR_INP    0
 
 #define WAIT_SINGLE() asm volatile ("nop")
 
-const uint16_t CHIP_ID_ENTRY[][2] {
+#define WAIT_TCE() delay(100)
+#define WAIT_TSE() delay(25)
+#define WAIT_BYP() delayMicroseconds(20)
+
+typedef struct {
+  uint32_t addr;
+  uint8_t data;
+} cmd_step;
+
+cmd_step CHIP_ID_ENTRY[] = {
   {0x5555, 0xAA},
   {0x2AAA, 0x55},
   {0x5555, 0x90}
 };
 
-void setAddr(uint32_t addr) {
-  DDRC = 0xff;
-  PORTC = addr & 0xff;
-  DDRL = 0xff;
-  PORTL = (addr >> 8) & 0xff;
-  DDRC |= 0b111;
-  PORTC = (addr >> 16) & 0b111;
+cmd_step CHIP_ID_EXIT[] = {
+  {0x5555, 0xAA},
+  {0x2AAA, 0x55},
+  {0x5555, 0xF0}
+};
+
+cmd_step CHIP_ERASE[]  = {
+  {0x5555, 0xAA},
+  {0x2AAA, 0x55},
+  {0x5555, 0x80},
+  {0x5555, 0xAA},
+  {0x2AAA, 0x55},
+  {0x5555, 0x10},
+};
+
+cmd_step CHIP_WRITE[] = {
+  {0x5555, 0xAA},
+  {0x2AAA, 0x55},
+  {0x5555, 0xA0}
+};
+
+
+void programmer_init()
+{
+  /* ADDRESS# + CE# */
+  DDR_ADDR_L = 0xffu;
+  DDR_ADDR_M = 0xffu;
+  DDR_ADDR_H = 0x07u;
+
+  /* DATA# */
+  DDR_DATA_L = 0xffu;
+
+  /* OE# , WE# */
+  DDR_WE = MASK_WE;
+  DDR_OE = MASK_OE;
+
+  PORT_WE |= MASK_WE;
+  PORT_OE |= MASK_OE;
 }
 
-void dataOut(byte data) {
-  DDRA = 0b11111111;
-  PORTA = data;
+void set_addr(uint32_t addr)
+{
+  addr &= 0x7FFFFu;
+  PORT_ADDR_L = ((addr >>  0u) & 0xffu);
+  PORT_ADDR_M = ((addr >>  8u) & 0xffu);
+  PORT_ADDR_H = ((addr >> 16u) & 0x07u);
 }
 
-byte dataIn() {
-  DDRA = 0;
-  return PINA;
+void out_data(uint8_t data)
+{
+  DDR_DATA_L = 0xFFu;
+  WAIT_SINGLE();
+  WAIT_SINGLE();
+  PORT_DATA_L = data;
 }
 
-void write_cycle(uint16_t addr, uint8_t data) {
-  SET_OE(HIGH);
-  SET_WE(HIGH);
+uint8_t inp_data()
+{
+  DDR_DATA_L = 0x00u;
+  PORT_DATA_L = 0x00u;
   WAIT_SINGLE();
+  WAIT_SINGLE();
+  return PINP_DATA_L;
+}
 
-  setAddr(addr);
-  SET_WE(LOW);
+void set_we(bool b)
+{
+  if (!b) PORT_WE |= MASK_WE;
+  else PORT_WE &= ~MASK_WE;
+}
+
+void set_oe(bool b)
+{
+  if (!b) PORT_OE |= MASK_OE;
+  else PORT_OE &= ~MASK_OE;
+}
+
+void write_cycle(uint32_t addr, uint8_t data)
+{
+  set_oe(0);
+  set_we(0);
   WAIT_SINGLE();
-  dataOut(data);
-  SET_WE(HIGH);
+  set_addr(addr);
+  set_we(1);
+  WAIT_SINGLE();
+  out_data(data);
+  set_we(0);
   WAIT_SINGLE();
 }
 
-byte read_cycle(uint32_t addr) {
-  SET_WE(HIGH);
-  SET_OE(HIGH);
+uint8_t read_cycle(uint32_t addr)
+{
+  set_we(0);
+  set_oe(1);
   WAIT_SINGLE();
-
-  setAddr(addr);
-  SET_OE(LOW);
+  set_addr(addr);
   WAIT_SINGLE();
-
-  byte r = dataIn();
-  WAIT_SINGLE();
-  WAIT_SINGLE();
+  byte r = inp_data();
   return r;
 }
 
-void chipId() {
-  write_cycle(0x5555, 0xaa);
-  write_cycle(0x2aaa, 0x55);
-  write_cycle(0x5555, 0x90);
+void writeCmd(cmd_step cmd[], int steps) {
+  for (int i = 0; i < steps; i++) {
+    write_cycle(cmd[i].addr, cmd[i].data);
+  }
+  WAIT_SINGLE();
+}
 
-  delay(10);
+uint16_t chipId() {
+  writeCmd(CHIP_ID_ENTRY, 3);
+  word a = read_cycle(0x0000u);
+  word b = read_cycle(0x0001u);
 
-  Serial.println(read_cycle(0x0), HEX);
+  writeCmd(CHIP_ID_EXIT, 3);
+  return (a << 8) | b;
+}
+
+uint8_t read8() {
+  while (Serial.available() <= 0) {
+    continue;
+  }
+  return Serial.read();
+}
+
+uint32_t read32() {
+  return (read8() << 24) | (read8() << 16) | (read8() << 8) | read8();
+}
+
+void chip_erase() {
+  writeCmd(CHIP_ERASE, 6);
+  WAIT_TCE();
+}
+
+void sector_erase(dword seca) {
+  seca <<= 12;
+  write_cycle(0x5555u, 0xAAu);
+  write_cycle(0x2AAAu, 0x55u);
+  write_cycle(0x5555u, 0x80u);
+  write_cycle(0x5555u, 0xAAu);
+  write_cycle(0x2AAAu, 0x55u);
+  write_cycle(seca,    0x30u);
+  WAIT_TSE();
+}
+
+void sector_read(byte (&buff)[BUFFER_SIZE], dword addr)
+{
+  memset(buff, 0, BUFFER_SIZE);
+  for(dword a = 0; a < BUFFER_SIZE; ++a)
+    buff[a] = read_cycle(addr+a);
+}
+
+void program_byte(dword addr, byte data)
+{
+  for (int i = 0; i < 3; i++) {
+    write_cycle(CHIP_WRITE[i].addr, CHIP_WRITE[i].data);
+  }
+  write_cycle(addr, data);
+  WAIT_BYP();
+}
+
+uint32_t parse32(uint8_t *data) {
+  return (*data) | (*(data + 1)) | (*(data+2)) | (*(data+3));
+}
+
+#define CHUNK_SIZE 1024
+static uint8_t buf[CHUNK_SIZE];
+
+uint32_t write_seq(uint8_t *buf, uint32_t size, uint32_t start) {
+  for (int i = 0; i < size; i++) {
+    program_byte(start+i, buf[i]);
+  }
+  return start + size;
 }
 
 void setup() {
-  pinMode(OE, OUTPUT);
-  pinMode(WE, OUTPUT);
-
+  programmer_init();
   Serial.begin(115200);
 
+  writeCmd(CHIP_ID_EXIT, 3);
+  inp_data();
   chipId();
 }
 
-void loop() {}
+void loop() {
+  if (Serial.available() == 0) {
+    return;
+  }
+
+  byte cmd = Serial.read();
+
+  if (cmd == 'c') {
+      uint16_t id = chipId();
+      Serial.write(id >> 8);
+      Serial.write(id & 0xff);
+      return;
+  } else if (cmd == 'e') {
+    chip_erase();
+    Serial.write(0xff);
+  } else if (cmd == 'w') {
+      WAIT_SINGLE();
+
+      uint8_t buf[5];
+      Serial.readBytes(buf, 5);
+
+      uint32_t len = parse32(buf);
+
+      Serial.println(len);
+      uint8_t mode = buf[4];
+      if (mode == 'a') {
+        for (uint32_t i = 0; i < len; i++) {
+          Serial.readBytes(buf, 5);
+
+          uint32_t addr = parse32(buf);
+          uint8_t  data = buf[4];
+
+          program_byte(addr, data);
+        }
+      } else if (mode == 's') {
+        uint32_t addr = 0;
+        for (uint32_t i = 0; i < (len/CHUNK_SIZE); i++) {
+          while (Serial.available() <= 0) {
+            continue;
+          }
+          Serial.readBytes(buf, CHUNK_SIZE);
+          addr = write_seq(buf, CHUNK_SIZE, addr);
+        }
+
+        if (len % CHUNK_SIZE != 0) {
+          while (Serial.available() <= 0) {
+            continue;
+          }
+          Serial.readBytes(buf, len % CHUNK_SIZE);
+          Serial.println(buf[0]);
+          write_seq(buf, len % CHUNK_SIZE, addr);
+        }
+      }
+
+      Serial.write((len >> 24) & 0xff);
+      Serial.write((len >> 16) & 0xff);
+      Serial.write((len >> 8) & 0xff);
+      Serial.write(len & 0xff);
+  } else if (cmd == 'r') {
+    uint32_t len = read32();
+    uint8_t mode  = read8();
+
+    if (mode == 'a') {
+      for (uint32_t i = 0; i < len; i++) {
+        uint32_t addr = read32();
+        Serial.write(read_cycle(addr));
+      }
+    } else if (mode == 's') {
+      for (uint32_t i = 0; i < len; i++) {
+        Serial.write(read_cycle(i));
+      }
+    }
+  } else if (cmd == 't') {
+      Serial.println(read_cycle(0));
+      Serial.println(read_cycle(1));
+    }
+}
