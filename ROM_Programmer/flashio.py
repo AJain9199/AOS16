bce53448861d0ab3c74760be32980ebf45ac3b61
Aryaman Jain <aryamanjain9199@gmail.com>
2025-02-21 18:16:13 +0530

    Completed flash I/O

--- ROM_Programmer/flashio.py
+++ ROM_Programmer/flashio.py
@@ -8,6 +8,12 @@ from tqdm import tqdm
 
 CHUNK_SIZE = 4096
 
+chip_id = {
+    0xB5: "SST39SF010A",
+    0xB6: "SST39SF020A",
+    0xB7: "SST39SF040"
+}
+
 def read8(srl):
     return int.from_bytes(srl.read(1))
 
@@ -18,19 +24,17 @@ def read32(srl):
 
 def manufacturer_id_handshake(srl):
     srl.write('c'.encode())
-    time.sleep(0.5)
     manufacturer_id = read8(srl)
-    chip_id = read8(srl)
+    chip_id_read = read8(srl)
 
-    if manufacturer_id == 0xBF and chip_id == 0xB5:
-        print("Manufacturer handshake successful")
+    if manufacturer_id == 0xBF and chip_id_read in chip_id.keys():
+        print(f"Manufacturer handshake successful.\nChip is {chip_id[chip_id_read]}.")
     else:
         return
 
 
 def chip_erase(srl):
     srl.write('e'.encode())
-    time.sleep(0.5)
     if read8(srl) == 0xff:
         print("Chip erased.")
     else:
@@ -45,28 +49,10 @@ def write8(srl, n):
     srl.write(n)
 
 
-def write_data(srl, data):
-    srl.write('w'.encode())
-    srl.write(len(data).to_bytes(4, 'big'))
-    srl.write('a'.encode())
-
-    bytedata = []
-
-    for (addr, byte) in data:
-        bytedata.extend(addr.to_bytes(4, 'big'))
-        bytedata.extend(byte.to_bytes(1))
-
-    srl.write(bytedata)
-
-    return read32(srl) == len(data)
-
-
 def write_seq_chunk(srl, b, nb=0):
     srl.write('w'.encode())
     srl.write(len(b).to_bytes(4, 'big'))
     srl.write(nb.to_bytes(4, 'big'))
-
-    time.sleep(0.5)
 
     srl.write(b)
 
@@ -107,38 +93,43 @@ def read_seq(srl, n, start=0):
     srl.reset_input_buffer()
     srl.reset_output_buffer()
 
-    time.sleep(0.5)
     return srl.read(n)
 
 
-if __name__ == "__main__":
-    # manufacturer_id()
+def init_args():
+    parser = ArgumentParser()
+    parser.add_argument('-r', type=int, default=2048, help='Read from the flash sequentially.')
+    parser.add_argument('-o', type=argparse.FileType('wb'), help='Output file for data')
+    parser.add_argument('-w', type=argparse.FileType('rb'),
+                        help='Write to the flash sequentially using data from a file.')
+    parser.add_argument('-b', type=int, default=115200, help='Baud rate for serial communication. (default: 115200)')
+    parser.add_argument('-c', default='COM10', help='Serial port for communication. (default: COM10)')
+    return vars(parser.parse_args())
 
-    srl = serial.Serial('COM10')
-    srl.baudrate = 115200  # set Baud rate to 9600
+
+if __name__ == "__main__":
+    cargs = init_args()
+
+    srl = serial.Serial(cargs['c'])
+    srl.baudrate = cargs['b']  # set Baud rate to 9600
     srl.bytesize = 8  # Number of data bits = 8
     srl.parity = 'N'  # No parity
     srl.stopbits = 1  # Number of Stop bits = 1
 
     time.sleep(1)
 
-    parser = ArgumentParser()
-    parser.add_argument('-r', type=int, default=2048, help='Read from the flash sequentially.')
-    parser.add_argument('-w', type=argparse.FileType('rb'),
-                        help='Write to the flash sequentially using data from a file.')
-    parser.add_argument('-b', type=int, default=115200, help='Baud rate for serial communication. (default: 115200)')
-    parser.add_argument('-c', default='COM10', help='Serial port for communication. (default: COM10)')
-    args = parser.parse_args()
-    args = vars(args)
+    manufacturer_id_handshake(srl)
 
-    if args.get('w'):
+    if cargs.get('w'):
         chip_erase(srl)
 
         print("Write begun.")
-        b = write_seq(srl, args['w'])
+        b = write_seq(srl, cargs['w'])
         if not b:
             print("Write failed.")
             exit(1)
 
         print(f"{b} bytes written and verified successfully")
-    # main()
+        cargs['w'].close()
+    elif cargs.get('r'):
+        print("Read begun.")
