c7662d2908936d0adbeaf4199ef083a9c0dbbe6a
Aryaman Jain <aryamanjain9199@gmail.com>
2025-10-23 23:46:31 +0530

    Completed instruction refactor

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -11,21 +11,22 @@ void init_opcodes() {
     add_opcode("add", 0x0, MOV_LIKE);
     add_opcode("sub", 0x1, MOV_LIKE);
     add_opcode("and", 0x2, MOV_LIKE);
-    add_opcode("or", 0x3, MOV_LIKE);
-    add_opcode("nor", 0x4, MOV_LIKE);
-    add_opcode("xor", 0x5, MOV_LIKE);
+    add_opcode("nand", 0x3, MOV_LIKE);
+    add_opcode("or", 0x4, MOV_LIKE);
+    add_opcode("nor", 0x5, MOV_LIKE);
+    add_opcode("xor", 0x6, MOV_LIKE);
     // add_opcode("neg", 0x6, SINGLE_ARG);
-    add_opcode("not", 0x7, SINGLE_ARG);
-    add_opcode("rsh", 0x8, SINGLE_ARG);
-    add_opcode("inc", 0x9, SINGLE_ARG);
-    add_opcode("dec", 0xa, SINGLE_ARG);
-    add_opcode("cmp", 0xb, MOV_LIKE);
-    add_opcode("mov", 0x6, MOV_LIKE);
-    add_opcode("push", 0xd, SINGLE_ARG_ALL);
-    add_opcode("pop", 0xe, SINGLE_ARG);
-    add_opcode("swp", 0xf, NO_ARGS);
-    add_opcode("jmp", 0x10, {REG | IMMEDIATE});
-    add_opcode("jnz", 0x11, {REG | IMMEDIATE});
+    add_opcode("not", 0x8, SINGLE_ARG);
+    add_opcode("shl", 0x9, SINGLE_ARG);
+    add_opcode("inc", 0xa, SINGLE_ARG);
+    add_opcode("dec", 0xb, SINGLE_ARG);
+    add_opcode("cmp", 0xc, MOV_LIKE);
+    add_opcode("mov", 0xd, MOV_LIKE);
+    add_opcode("push", 0xe, SINGLE_ARG_ALL);
+    add_opcode("pop", 0xf, SINGLE_ARG);
+    add_opcode("swp", 0x10, NO_ARGS);
+    add_opcode("jmp", 0x11, {REG | IMMEDIATE});
+    add_opcode("jnz", 0x12, {REG | IMMEDIATE});
 }
 
 int main() {
@@ -33,6 +34,8 @@ int main() {
     add_register("ax", 0x0);
 	add_register("bx", 0x1);
 	add_register("cx", 0x2);
+    add_register("dx", 0x3);
+    add_register("ex", 0x4);
 
     Parser parser_engine("test.S");
     parser_engine.parse();
